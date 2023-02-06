1826ec7a550a4be4c7832ac960ab6b3d9b933602
holydanchik <tuzelbaeff@gmail.com>
2023-02-07 00:22:30 +0600

    lab3 functinons1 commit

--- lab3/functions1/task13.py
+++ lab3/functions1/task13.py
@@ -1,28 +1,21 @@
 from random import randrange
 
-def game (guess, number):
-    i = 1
-    while (guess != number):
-        print('Take a guess.')
-        guess = int(input())
-        print()
-        if guess == number:
-            print('Good job, ' + name + '! You guessed my number in', i, 'guesses!')
-        elif guess>number:
-            i += 1
-            print('Your guess is too high.')
-        elif number>guess:
-            i += 1
-            print('Your guess is too low.')
+def game (guess, number, count):
+    print('Take a guess.')
+    guess = int(input())
+    print()
+    if guess == number:
+        print('Good job, ' + name + '! You guessed my number in', count,  'guesses!')
+    else:
+        print('Your guess is too high.') if number<guess else print('Your guess is too low.') 
+        game(guess, number, count+1)
     
-
 print('Hello! What is your name?')
 name = input()
 print()
 print("Well, " + name + ", I am thinking of a number between 1 and 20.")
 
-
 number = randrange(1, 20)
 guess = 21
 
-game(guess, number)
+game(guess, number, 1)
